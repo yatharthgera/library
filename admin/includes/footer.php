417aa4eedce51b170f36df1cca4b1f0549b64fdc
yatharthgera <gera.yatharth@gmail.com>
2020-09-06 03:10:34 +0530

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,14 @@
+<section class="footer-section">
+   <div class="row">
+   
+   <div class="col-md-12">
+   <h3>
+
+       
+    &copy; 2020 VIT Online Library Management System | Designed by : YATHARTH GERA 19BCE1379    |    ANUPAM KUNWAR  19BCE1369</a> 
+    </h3>
+    </div>
+
+    </div>
+        </div>
+    </section>
